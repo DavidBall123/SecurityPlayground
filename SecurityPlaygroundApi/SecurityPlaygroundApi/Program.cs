de2ceafa41e6f9a870e21a8122c36286af61ffa8
David Ball <david.ball@agriculture.gov.au>
2022-12-21 09:15:56 +1100

    Add CORS policy

--- SecurityPlaygroundApi/SecurityPlaygroundApi/Program.cs
+++ SecurityPlaygroundApi/SecurityPlaygroundApi/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddPolicy("ApiCorsPolicy", builder =>
+{
+    builder.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -19,6 +23,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("ApiCorsPolicy");
 
 app.MapControllers();
 
