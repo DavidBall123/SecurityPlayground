4ab904258e2ba3de370f5fc966e3350df8702597
David Ball <david.ball@agriculture.gov.au>
2022-12-22 08:53:25 +1100

    Added domain classes

---
+++ SecurityPlaygroundApi/SecurityPlaygroundApi/DAL/Class.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using SecurityPlaygroundApi.Model;
+
+namespace SecurityPlaygroundApi.DAL
+{
+    public class CertificateContext : DbContext
+    {
+        public CertificateContext() : base("CertificateContext")
+        {
+
+        }
+
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ SecurityPlaygroundApi/SecurityPlaygroundApi/Migrations/202212212148287_InitialCreate.cs
@@ -0,0 +1,44 @@
+﻿namespace SecurityPlaygroundApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Certificate",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Acronym = c.String(),
+                        Organisation = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Subject",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CertificateId = c.Int(nullable: false),
+                        Name = c.String(),
+                        Weight = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Certificate", t => t.CertificateId, cascadeDelete: true)
+                .Index(t => t.CertificateId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Subject", "CertificateId", "dbo.Certificate");
+            DropIndex("dbo.Subject", new[] { "CertificateId" });
+            DropTable("dbo.Subject");
+            DropTable("dbo.Certificate");
+        }
+    }
+}
